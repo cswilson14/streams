bcbe914bb5fef8ef0078b08a3cbc75304d85fd04
chris <larco1993@gmail.com>
2019-10-29 22:29:31 -0400

    Added Routes for React-Router
    Created the Route components for each page we want to display.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-
-const PageOne = () => {
-    return <div><Link to="/pagetwo">Navigate to page two</Link></div>;
-}
-
-const PageTwo = () => {
-    return <div><Link to="/pagetwo">Navigate to page one</Link></div>;
-}
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = (props) => {
     return (
         <div>
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne} />
-                    <Route path="/" component={PageTwo} />
+                <Route path="/" exact component={StreamList} />
+                <Route path="/streams/new" exact component={StreamCreate} />
+                <Route path="/streams/edit" exact component={StreamEdit} />
+                <Route path="/streams/delete" exact component={StreamDelete} />
+                <Route path="/streams/show" exact component={StreamShow} />
                 </div>
             </BrowserRouter>
         </div>
