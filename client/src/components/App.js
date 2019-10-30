35c915591bbc6820e71104d3dc05341d515a68c5
chris <larco1993@gmail.com>
2019-10-29 22:14:48 -0400

    Added Scaffolding for React-Router
    Created a couple of scaffolding components to be used with
    react-router for navigating with URLs.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+    return <div><Link to="/pagetwo">Navigate to page two</Link></div>;
+}
+
+const PageTwo = () => {
+    return <div><Link to="/pagetwo">Navigate to page one</Link></div>;
+}
 
 const App = (props) => {
-    return <div>App</div>;
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne} />
+                    <Route path="/" component={PageTwo} />
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
