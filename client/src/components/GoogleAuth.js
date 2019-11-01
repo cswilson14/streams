233db50d1d6296d72c27dc2fdf362602d41966c8
chris <larco1993@gmail.com>
2019-10-31 20:08:24 -0400

    Add sign in/sign out
    Here we add a styled "Sign in with Google" button and a "Sign Out"
    button, enabling us to sign in and out.

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -18,15 +18,33 @@ class GoogleAuth extends React.Component {
 
     onAuthChange = () => {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    };
+
+    onSignIn = () => {
+        this.auth.signIn();
+    };
+
+    onSignOut = () => {
+        this.auth.signOut();
     }
 
     renderAuthButton() {
         if(this.state.isSignedIn === null) {
-            return <div>I don't know if we are signed in</div>;
+            return null;
         } else if (this.state.isSignedIn) {
-            return <div>I am signed in!</div>;
+            return (
+                <button onClick={this.onSignOut} className="ui red google button">
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            );
         } else {
-            return <div>I am not signed in</div>;
+            return (
+                <button onClick={this.onSignIn} className="ui red google button">
+                    <i className="google icon" />
+                    Sign In with Google
+                </button>
+            );
         }
     }
 
